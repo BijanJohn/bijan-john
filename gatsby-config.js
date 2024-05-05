36665bc7bd9480b8d8895631f0cc3ddd7a4e8089
Bijan <34772523+BijanJohn@users.noreply.github.com>
2024-05-05 05:41:22 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `bijan-john`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
