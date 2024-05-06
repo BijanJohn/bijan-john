c80a60c9f0a21cf9b9328fdb2a4b15d0fea8dc18
Bijan <34772523+BijanJohn@users.noreply.github.com>
2024-05-05 20:17:32 -0500

    added graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,18 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `bijan-john`,
-    siteUrl: `https://www.bijanjohn.com`,
+    title: "Artistry and Blogging by Bijan John",
+    siteUrl: "https://www.bijanjohn.com",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
   ],
 }
